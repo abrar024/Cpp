5bf2e09d7d65a42b9c324e1d2af3b35a18ab5296
abrar024 <97375184+abrar024@users.noreply.github.com>
2022-01-09 10:45:42 +0530

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+    int sum = 0;
+    int A[] = { 2,4,6,7,9 };
+
+    for (int x : A)
+    {
+        sum = sum + x;
+        std::cout << x << std::endl;
+    }
+
+    std::cout << "sum is " << sum << std::endl;
+}

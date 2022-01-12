3dadb93ee3f3a340ba833433b4fcc387458fff76
abrar024 <97375184+abrar024@users.noreply.github.com>
2022-01-12 22:51:11 +0530

    Create enum.cpp

---
+++ enum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+enum day { mon, tue, wed, thy, fri };
+
+
+int main()
+{
+	day d;
+	d = wed;
+	cout << d << endl;
+
+	return 0;
+
+}
+
+output=2
+  
+  it will auto. assign no. to list.

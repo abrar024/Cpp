7643eb94c104693d00a85b33d15d9851f6f9bf72
abrar024 <97375184+abrar024@users.noreply.github.com>
2022-01-14 00:45:38 +0530

    Create relation operators.cpp

---
+++ relation operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cout << "enter 2 no.";
+	cin >> x >> y;
+	
+	if (x > y)
+	{
+		cout << "max is"<< x ;
+	}
+	else
+	{
+		cout << "nax is" << y;
+	}
+	return 0;
+}

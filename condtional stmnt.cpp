9bc7a0b24c76af4e60b8e8e47cfafcd59d2fbd77
abrar024 <97375184+abrar024@users.noreply.github.com>
2022-01-16 13:56:51 +0530

    Create condtional stmnt.cpp

---
+++ condtional stmnt.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int hour;
+	cout << "enter hours";
+	cin >>hour;
+	if (hour >= 9 && hour <= 18)
+		cout << "working";
+	else
+		cout << "leisure";
+
+		return 0;
+}
